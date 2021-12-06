9c86b6a3e7049fd3e6c678d8f76d4e24ba9d3b9e
Gforcedev <tecshotclark@gmail.com>
2021-12-06 21:30:22 +0000

    Add my attempt at day 6

--- src/aoc/6.clj
+++ src/aoc/6.clj
@@ -1,7 +1,43 @@
 (defn parse-input [input]
   (as-> input v
-       (clojure.string/split v #",")
-       (mapv #(Integer/parseInt %) v)))
+    (slurp v)
+    (clojure.string/replace v #"\n" "")
+    (clojure.string/split v #",")
+    (mapv #(Integer/parseInt %) v)))
 
-(parse-input "1,2,3,4,5")
+(defn do-day [fish-list]
+  (->> fish-list
+       (map dec)
+       (replace {-1 [6 8]})
+       flatten
+       vec))
+
+; Part 1
+(->> "assets/input6.txt"
+     parse-input
+     ((apply comp (repeat 80 do-day)))
+     count)
+
+; Part 2 experimentation
+(defn parse-input-2 [input]
+  (as-> input v
+    (slurp v)
+    (clojure.string/replace v #"\n" "")
+    (clojure.string/split v #",")
+    (mapv #(Integer/parseInt %) v)
+    (mapv vector v)))
+
+(defn do-day-2 [fish-list-list]
+  (mapv #(do-day %) fish-list-list))
+
+(->> "assets/input6.txt"
+     parse-input-2
+     ((apply comp (repeat 64 do-day-2))))
+
+(sort-by key (frequencies (map #(->> "1"
+     parse-input
+     ((apply comp (repeat % do-day)))
+     count) (vec (range 64)))))
+
+; Yeah I don't think I'm managing part 2
 
