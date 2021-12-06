c434fc541761f338bf2cc70d536ff2362cc34790
Gforcedev <tecshotclark@gmail.com>
2021-12-06 21:56:16 +0000

    Re-implement someone else's solution for day 6 because it's so neat

--- src/aoc/6.clj
+++ src/aoc/6.clj
@@ -41,3 +41,18 @@
 
 ; Yeah I don't think I'm managing part 2
 
+; A different solution found from https://github.com/zelark/AoC-2021/blob/main/src/zelark/aoc_2021/day_06.clj
+; Which I saw one tiny bit of and decided I wanted to implement
+(def better-input (->> "assets/input6.txt"
+     parse-input
+     (frequencies)
+     (sort-by key)))
+
+(def starting-point
+  (reduce (fn [curr [amount timer]]
+            (assoc curr amount timer)) (vec (range 9)) better-input))
+
+(defn do-day-better [vals]
+    (assoc (apply vector (flatten [(rest vals) (first vals)])) 6 (first vals)))
+
+(do-day-better starting-point)
